d714cd4e4d4a26bc734e4be908dfe24b5a0ddd16
danadube <dana@danadube.com>
2025-07-09 09:15:02 -0700

    Initial Commit

---
+++ Glaab Farm CRM/Glaab_Farm_CRMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Glaab_Farm_CRMApp.swift
+//  Glaab Farm CRM
+//
+//  Created by Dana Dube on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Glaab_Farm_CRMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Glaab Farm CRMTests/Glaab_Farm_CRMTests.swift
@@ -0,0 +1,17 @@
+//
+//  Glaab_Farm_CRMTests.swift
+//  Glaab Farm CRMTests
+//
+//  Created by Dana Dube on 7/9/25.
+//
+
+import Testing
+@testable import Glaab_Farm_CRM
+
+struct Glaab_Farm_CRMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
